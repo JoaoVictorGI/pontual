a8e5a2e20831909c657931075cfee7658f3cbbf1
João Victor Gomes Izidoro <joaovictor.gizidoro@gmail.com>
2026-06-10 16:56:37 -0300

    Initialize the project

---
+++ src/test/java/xyz/usepontual/pontual/TestPontualApplication.java
@@ -0,0 +1,11 @@
+package xyz.usepontual.pontual;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPontualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(PontualApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}

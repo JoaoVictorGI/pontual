5ba59da75fda632bbfdfcaf83f035f3249012f66
João Victor Gomes Izidoro <joaovictor.gizidoro@gmail.com>
2026-06-11 18:59:13 -0300

    feat: add code formatting using spotless for pom.xml, java and sql files

--- src/test/java/xyz/usepontual/pontual/TestPontualApplication.java
+++ src/test/java/xyz/usepontual/pontual/TestPontualApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 public class TestPontualApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.from(PontualApplication::main).with(TestcontainersConfiguration.class).run(args);
+    static void main(String[] args) {
+        SpringApplication.from(PontualApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
     }
-
 }

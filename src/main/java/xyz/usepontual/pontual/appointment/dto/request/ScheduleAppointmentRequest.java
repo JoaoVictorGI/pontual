7a94e0d6bcf22a9b50cd5a7c9769808754cd62bf
João Victor Gomes Izidoro <joaovictor.gizidoro@gmail.com>
2026-06-18 17:20:59 -0300

    Validate if startsAt and endsAt if future using @Future annotation

--- src/main/java/xyz/usepontual/pontual/appointment/dto/request/ScheduleAppointmentRequest.java
+++ src/main/java/xyz/usepontual/pontual/appointment/dto/request/ScheduleAppointmentRequest.java
@@ -1,12 +1,13 @@
 package xyz.usepontual.pontual.appointment.dto.request;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import java.time.Instant;
 import java.util.UUID;
 
 public record ScheduleAppointmentRequest(
-        @NotBlank UUID professionalId,
-        @NotBlank UUID clientId,
-        @NotEmpty Instant startsAt,
-        @NotEmpty Instant endsAt) {}
+        @NotBlank UUID providerId,
+        @NotBlank UUID customerId,
+        @NotEmpty @Future Instant startsAt,
+        @NotEmpty @Future Instant endsAt) {}

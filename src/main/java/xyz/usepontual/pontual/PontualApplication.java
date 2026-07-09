8b344da91d3b5d3bface4dc0e58e7be2bff02760
João Victor Gomes Izidoro <joaovictor.gizidoro@gmail.com>
2026-07-09 17:12:17 -0300

    Create project

---
+++ src/main/java/xyz/usepontual/pontual/PontualApplication.java
@@ -0,0 +1,13 @@
+package xyz.usepontual.pontual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontualApplication.class, args);
+    }
+
+}
